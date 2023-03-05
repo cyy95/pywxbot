05235304d9ce3e2bf0036f849820d57335505b82
cyy95 <949777915@qq.com>
2023-03-05 15:35:09 +0800

    poc

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from typing import List
 
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as fh:
-    long_description = fh.read()
+
+def get_long_description() -> str:
+    """get long_description"""
+    with open('README.md', 'r', encoding='utf-8') as readme_fh:
+        return readme_fh.read()
 
 
 def get_install_requires() -> List[str]:
     """get install_requires"""
-    with open('requirements.txt', 'r') as requirements_fh:
+    with open('requirements.txt', 'r', encoding='utf-8') as requirements_fh:
         return requirements_fh.read().splitlines()
 
 
@@ -18,16 +21,17 @@ setuptools.setup(
     author="cyy95",
     author_email="949777915@qq.com",
     description="A wechat bot SDK for windows",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/cyy95/wxbot-win",
-    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows",
+        "Operating System :: Microsoft :: Windows",
     ],
-    install_requires=get_install_requires(),
+    install_requires=['pywinauto', 'psutil', 'pyperclip', 'pyautogui'],
     python_requires='>=3.6',
     package_dir={'wxbot-win': 'src/wxbot-win'},
+    packages=setuptools.find_packages(where='src', exclude=['__pycache__', '.mypy_cache']),
+    platforms=['Windows'],
 )
